b928525852fcae2914ed2b625a65aea0863aaf4b
Marcos <marcos.alvarenga@10pines.com>
2020-09-06 19:35:33 -0300

    Permited all requests

---
+++ src/main/java/edu/ar/unq/qubobe/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package edu.ar.unq.qubobe.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    securedEnabled = true,
+    jsr250Enabled = true,
+    prePostEnabled = true
+)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+            .frameOptions()
+            .disable()
+            .and()
+            .authorizeRequests()
+            .anyRequest().permitAll()
+            .and()
+            .csrf()
+            .disable()
+            .formLogin()
+            .disable()
+            .httpBasic()
+            .disable();
+    }
+}
