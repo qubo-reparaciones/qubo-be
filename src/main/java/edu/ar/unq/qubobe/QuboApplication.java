ea23ad8aea20c0da5bda6e3a0856c8ccf6dae184
Marcos <marcos.alvarenga@10pines.com>
2020-09-06 00:58:57 -0300

    Init project

---
+++ src/main/java/edu/ar/unq/qubobe/QuboApplication.java
@@ -0,0 +1,13 @@
+package edu.ar.unq.qubobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ar/unq/qubobe/QuboApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ar.unq.qubobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
